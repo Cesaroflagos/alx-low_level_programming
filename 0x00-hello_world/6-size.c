8bcee434044a3804d5d91591a01a265f6f89f9ec
Cesaroflagos <yusufayoola5@gmail.com>
2023-06-15 20:40:52 -0700

    Size and Territory not enough to make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- * main - A programm that prints the size of various types on the compu
-ter
+ * main - Prints the size of various comuter types
  * Return: 0 (Done)
  */
+
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f);
 return (0);
 }
