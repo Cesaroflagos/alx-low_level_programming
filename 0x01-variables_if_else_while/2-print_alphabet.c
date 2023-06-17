90c90d23314d9f43363a9a92dbe8124b391f865b
Cesaroflagos <yusufayoola5@gmail.com>
2023-06-16 19:35:03 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Return: Repeat 0 (Done)
+ */
+
+int main(void)
+{
+	char cr;
+
+	cr = 'a';
+	while
+		(cr <= 'z') {
+			putchar(cr);
+			cr++;
+		}
+	putchar('\n');
+	return (0);
+}
