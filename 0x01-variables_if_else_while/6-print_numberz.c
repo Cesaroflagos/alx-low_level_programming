8614c22b9ab1c6f688d6b6a55474fa2510e16191
Cesaroflagos <yusufayoola5@gmail.com>
2023-06-17 09:38:40 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Starting point
+ * Return: Repeat 0 (Done)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 9; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
