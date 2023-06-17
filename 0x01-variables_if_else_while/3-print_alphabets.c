6da6c980f21ff043105f2e1e2bfaac3375652a98
Cesaroflagos <yusufayoola5@gmail.com>
2023-06-16 19:50:22 -0700

    Manipulating Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Return: Repeat 0 (Done)
+ */
+
+int main(void)
+{
+	char cr;
+
+	char hr;
+
+	cr = 'a';
+	hr = 'A';
+	while
+		(cr <= 'z') {
+			putchar(cr);
+			cr++;
+		}
+	while
+		(hr <= 'z') {
+			putchar(hr);
+			hr++;
+		}
+	putchar('\n');
+	return (0);
+}
