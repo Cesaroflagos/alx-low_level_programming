8463cdd2010d707da809400c4b0c96a227ac9354
Cesaroflagos <yusufayoola5@gmail.com>
2023-06-18 06:56:52 -0700

    Size and Territory don't make a Nation

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10;
-
-	printf("Last digit of %d is %d", n, digit);
-	if (digit > 98)
 	{
-	printf("and is greater than 98\n");
+	printf("Last digit of %d is %d", n, digit);
+	}
+	if (digit > 5)
+	{
+	printf("and is greater than 5\n");
 	}
 	else if (digit == 0)
 	{
