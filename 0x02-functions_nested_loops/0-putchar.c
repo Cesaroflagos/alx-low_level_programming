9f902a007da1d78a81436f518a31cb9a2d0f9917
Cesaroflagos <yusufayoola5@gmail.com>
2023-06-20 14:43:48 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Starting point
+ * Return: Repeat 0 (Done)
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+return (0);
+}
