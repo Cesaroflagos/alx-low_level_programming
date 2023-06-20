d1e77a319aba4e4f43571f62ea860a4db4c45949
Cesaroflagos <yusufayoola5@gmail.com>
2023-06-20 15:04:13 -0700

    Lowercase game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char lc;
+
+for (lc = 'a'; lc <= 'z'; lc++)
+_putchar(lc);
+_putchar('\n');
+}
