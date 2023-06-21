82369bdc7651a7e6ef02c8f82330e136ac9ebad7
Cesaroflagos <yusufayoola5@gmail.com>
2023-06-20 19:59:37 -0700

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Return: 0
+ */
+int main(void)
+{
+int a = 0;
+long j = 1, c = 2;
+
+while (a < 50)
+{
+if (a == 0)
+printf("%ld", j);
+else if (a == 1)
+printf(", %ld", c);
+else
+{
+c += j;
+j = c - j;
+printf(", %ld", c);
+}
+++a;
+}
+printf("\n");
+return (0);
+}
