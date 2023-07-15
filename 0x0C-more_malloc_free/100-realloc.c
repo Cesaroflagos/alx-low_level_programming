4009ebcd2d1352a6aea83d78fa44d272d8c6657b
Cesaroflagos <yusufayoola5@gmail.com>
2023-07-14 18:38:07 -0700

    _realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - Starting point
+ * @ptr: character
+ * @old_size: character
+ * @new_size: character
+ * Return: NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL)
+ptr = malloc(new_size);
+
+if (new_size == old_size)
+return (ptr);
+free(ptr);
+ptr = malloc(new_size);
+
+return (ptr);
+}
